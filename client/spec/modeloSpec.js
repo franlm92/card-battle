db9746bb9d66de7a3dc1f72d14c6d6e4d7a46074
FRANCISCO LOPEZ MUNOZ <Fco.Lopez3@alu.uclm.es>
2018-10-01 13:49:17 +0200

    Añadimos a un segundo usuario, usr2

--- client/spec/modeloSpec.js
+++ client/spec/modeloSpec.js
@@ -1,10 +1,11 @@
 describe("El juego de las cartas...", function() {
   var juego;
-  var usr;
+  var usr1, usr2;
 
   beforeEach(function() {
     juego = new Juego();
-    usr = new Usuario("pepe");
+    usr1 = new Usuario("pepe");
+    usr2 = new Usuario("juan");
   });
 
   it("Debería tener una colección de cartas", function() {
@@ -15,20 +16,27 @@ describe("El juego de las cartas...", function() {
     expect(juego.usuarios.length).toEqual(0);
   });
 
-  it("El usuario debe tener un mazo", function(){
-    expect(usr.mazo).toBeDefined();
-    expect(usr.mazo.length).toEqual(0);
+  it("Los usuarios deben tener un mazo", function(){
+    expect(usr1.mazo).toBeDefined();
+    expect(usr1.mazo.length).toEqual(0);
+    expect(usr2.mazo).toBeDefined();
+    expect(usr2.mazo.length).toEqual(0);
   });
 
-  it("El usuario debe tener una mano inicialmente sin cartas", function(){
-    expect(usr.mano).toBeDefined();
-    expect(usr.mano.length).toEqual(0);
+  it("Los usuarios deben tener una mano inicialmente sin cartas", function(){
+    expect(usr1.mano).toBeDefined();
+    expect(usr1.mano.length).toEqual(0);
+    expect(usr2.mano).toBeDefined();
+    expect(usr2.mano.length).toEqual(0);
   });
 
-  it("Agrego al usuario al juego",function(){
-    juego.agregarUsuario(usr);
-    expect(juego.usuarios.length).toEqual(1);
+  it("Agrego a los usuarios al juego",function(){
+    juego.agregarUsuario(usr1);
+    juego.agregarUsuario(usr2);
+    expect(juego.usuarios.length).toEqual(2);
     expect(juego.usuarios[0].nombre).toEqual("pepe");
-    expect(usr.mazo.length).toEqual(30);
+    expect(juego.usuarios[1].nombre).toEqual("juan");
+    expect(usr1.mazo.length).toEqual(30);
+    expect(usr2.mazo.length).toEqual(30);
   })
 });
